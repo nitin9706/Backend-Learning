3746b74e9b8f4fc0adb9a67ee8db3cebdaf3e5ac
nitin-7781 <unknowngamer931@gmail.com>
2026-02-17 22:32:46 +0530

    finishing the setup to write the controllers

--- src/DB/database.js
+++ src/DB/database.js
@@ -1,14 +1,14 @@
-import mongoose, { connection, connections } from "mongoose";
+import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js     ";
 
 export const connectdb = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.DATABASE_URL}/${DB_NAME}`,
-      console.log(
-        "DB is connected sucessfuly",
-        connectionInstance.connection.host
-      )
+      `${process.env.DATABASE_URL}/${DB_NAME}`
+    );
+    console.log(
+      "DB is connected sucessfuly",
+      connectionInstance.connection.host
     );
   } catch (error) {
     console.log(`DataBase connection failed ${error}`);
--- src/app.js
+++ src/app.js
@@ -14,4 +14,15 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.route.js";
+
+//  declare route
+
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:3000/api/v1/users
+
 export { app };
